1ef1a1c80ddb454fc3dc4c2484abfac0cf41f8a9
vaadimvoronkov <vaadimvoronkov@gmail.com>
2024-08-15 18:25:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/reset.css";
+import "./css/main.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="header-logo">
+          <div className="header-logo__icon"></div>
+          <div className="header-logo__text">Portfolio</div>
+        </div>
+        <div className="header-nav">
+          <ul className="header-nav-list">
+            <li className="header-nav-list__item"><a href="#">Item1</a></li>
+            <li className="header-nav-list__item"><a href="#">Item2</a></li>
+            <li className="header-nav-list__item"><a href="#">Item3</a></li>
+          </ul>
+        </div>
+        
       </header>
+      <main className="section">
+
+      </main>
+      <footer className="footer">
+
+      </footer>
     </div>
   );
 }
