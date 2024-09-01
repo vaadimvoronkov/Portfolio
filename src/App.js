966c2f4399bba26cb24654363a243294562080ba
vaadimvoronkov <vaadimvoronkov@gmail.com>
2024-09-01 17:14:32 +0700

    second change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import "./css/reset.css";
 import "./css/main.css";
+import avatar from "./img/photo.jpg"
 
 function App() {
   return (
     <div className="App">
       <header className="header">
         <div className="header-logo">
-          <div className="header-logo__icon"></div>
-          <div className="header-logo__text">Portfolio</div>
+          <div className="header-logo__text"><b>Portfolio</b></div>
         </div>
         <div className="header-nav">
-          <ul className="header-nav-list">
-            <li className="header-nav-list__item"><a href="#">Item1</a></li>
-            <li className="header-nav-list__item"><a href="#">Item2</a></li>
-            <li className="header-nav-list__item"><a href="#">Item3</a></li>
-          </ul>
         </div>
-        
       </header>
       <main className="section">
-
+        <div className="section-card">
+          <div className="section-card-info">
+            <div className="section-card-info__photo">
+              <img className = "avatar" src= {avatar} alt="avatar"></img>
+            </div>
+            <div className="section-card-info__name"><u>Vadim Voronkov</u></div>
+            <div className="section-card-info__skills">Programmer</div>
+          </div>
+          <div className="section-card-bio">
+            <div className="section-card-bio__text"><u>Bio:</u><br></br>Father of 3 humans, 5 unicorns & 2 dogs,I design since I can remember it. I often get asked where I get my inspiration from: in everyday’s lil’ details. And sometimes in leftover food I find in my beard.</div>
+          </div>
+        </div>
       </main>
       <footer className="footer">
 
