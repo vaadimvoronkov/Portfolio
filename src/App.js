7df1b9d762ef9be6bb2b6e0e2bc9177b7c94dff2
vaadimvoronkov <vaadimvoronkov@gmail.com>
2024-09-01 17:29:06 +0700

    header nav deleted

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
             <div className="section-card-info__skills">Programmer</div>
           </div>
           <div className="section-card-bio">
-            <div className="section-card-bio__text"><u>Bio:</u><br></br>Father of 3 humans, 5 unicorns & 2 dogs,I design since I can remember it. I often get asked where I get my inspiration from: in everyday’s lil’ details. And sometimes in leftover food I find in my beard.</div>
+            <div className="section-card-bio__text"><u>Bio:</u><br></br>Some Information</div>
           </div>
         </div>
       </main>
