2f3707066f7ce6a89cdf9185b14ce2502f6668c1
vaadimvoronkov <vaadimvoronkov@gmail.com>
2024-10-01 11:42:12 +0700

    links adding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import "./css/reset.css";
 import "./css/main.css";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import { ProfilePage } from "./pages/ProfilePage/ProfilePage.js";
 import { MixcloudPage } from "./pages/MixcloudPage/MixcloudPage.js";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="header">
         <div className="header-logo">
-          <div className="header-logo__text"><b>Portfolio</b></div>
+          <div className="header-logo__text">
+            <b>Portfolio</b>
+          </div>
         </div>
-        <div className="header-nav">
-        </div>
+        <ul class="h__social">
+          <li>
+            <a href="https://t.me/vaadimvoronkov" class="telegram"></a>
+          </li>
+          <li>
+            <a href="https://vk.com/vaadimvoronkov" class="vk"></a>
+          </li>
+        </ul>
       </header>
       <ul className="navigation-block">
-          <li className="navigation-block-item"><Link to="/">Profile</Link></li>
-					<li className="navigation-block-item"><Link to="/mixcloud">MixCloud</Link></li>
-        </ul>
+        <li className="navigation-block-item">
+          <Link to="/">Profile</Link>
+        </li>
+        <li className="navigation-block-item">
+          <Link to="/mixcloud">MixCloud</Link>
+        </li>
+      </ul>
       <Routes>
         <Route path="/" element={<ProfilePage />} />
         <Route path="/mixcloud" element={<MixcloudPage />} />
