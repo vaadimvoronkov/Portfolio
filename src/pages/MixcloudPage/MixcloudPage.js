8780bbda8bbd7a28d31b29f3daf94e5ac4ea3bd0
vaadimvoronkov <vaadimvoronkov@gmail.com>
2024-10-12 14:21:41 +0700

    Разделение MixcloudPage на компоненты

--- src/pages/MixcloudPage/MixcloudPage.js
+++ src/pages/MixcloudPage/MixcloudPage.js
@@ -1,42 +1,10 @@
-import { useEffect, useState } from "react";
+import ReleaseItem from "../../components/MixcloudPage/ReleaseItem";
+import Pagination from "../../components/MixcloudPage/Pagination";
 import "./MixcloudPage.css";
-
-const apiUrl ="https://geschoss-sons-of-horus-59e6.twc1.net/mixcloud/releases?page=";
-const imageUrl = "https://aerostatbg.ru";
+import { useReleases } from "./hooks";
 
 export const MixcloudPage = () => {
-  const [releases, setReleases] = useState([]);
-  const [pager, setPager] = useState({});
-  const [isPlaying, setIsPlaying] = useState(null);
-  const [pageNumber, setPageNumber] = useState(0);
-
-  const buildUrl = (url, number) => {
-    return `${url}${number}`;
-  };
-  const handleNextPage = () => {
-    if (pageNumber < pager.total_pages) setPageNumber(pageNumber + 1);
-  };
-  const handlePrevPage = () => {
-    if (pageNumber > 0) setPageNumber(pageNumber - 1);
-  };
-  const handlePlayPause = (index) => {
-    if (isPlaying === index) {
-      setIsPlaying(null);
-    } else {
-      setIsPlaying(index);
-    }
-  };
-
-  useEffect(() => {
-    fetch(buildUrl(apiUrl, pageNumber))
-      .then((response) => response.json())
-      .then((data) => {
-        setReleases((prevReleases) => [...prevReleases, ...data.rows]);
-        setPager(data.pager);
-        console.log(data);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, [pageNumber]);
+  const { releases, pager, handleNextPage, handlePrevPage } = useReleases();
 
   if (!releases) {
     return <div>Загрузка...</div>;
@@ -46,56 +14,15 @@ export const MixcloudPage = () => {
     <div className="mixcloud-container">
       <ul className="release-list">
         {releases.map((release, index) => (
-          <li key={index} className="release-item">
-            <div className="release-header">
-              <h2>{release.title}</h2>
-              <p>{release.number}</p>
-              <div className="release-image-container" onClick={() => handlePlayPause(index)}>
-                <img src={`${imageUrl}${release.image_url}`} alt="" className="release-image"/>
-                {isPlaying === index ? (
-                  <div className="icon pause-icon">
-                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"className="pause-icon-svg">
-                      <rect x="6" y="4" width="4" height="16" />
-                      <rect x="14" y="4" width="4" height="16" />
-                    </svg>
-                  </div>
-                ) : (
-                  <div className="icon play-icon">
-                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="play-icon-svg">
-                      <polygon points="5,3 19,12 5,21" />
-                    </svg>
-                  </div>
-                )}
-              </div>
-            </div>
-            <div className="release-tracklist">
-              <h2>Список треков:</h2>
-              <div className="tracklist-compositions">
-                {release.composition_list.map((composition, index) => (
-                  <div key={index}>{composition.composition_name}</div>
-                ))}
-              </div>
-            </div>
-            <div className="release-info">
-              <p>Дата: {release.date}</p>
-              <p>Оценка: <span className="rating-number">{release.rating}</span></p>
-              <p>Количество оценок:{" "}<span className="vote-count-number">{release.vote_count}</span></p>
-            </div>
-          </li>
+          <ReleaseItem release={release} index={index}></ReleaseItem>
         ))}
       </ul>
-      <div className="pagination">
-        <button disabled={pager.current_page === 0} onClick={handlePrevPage} className="pagination-button">
-          Previous
-        </button>
-        <span className="pagination-info">
-          {pager.current_page + 1} / {pager.total_pages}
-        </span>
-        <button disabled={pager.current_page === pager.total_pages} onClick={handleNextPage} className="pagination-button">
-          Next
-        </button>
-      </div>
-      <div className="audio-player visible"></div>
+      <Pagination
+        pager={pager}
+        handleNextPage={handleNextPage}
+        handlePrevPage={handlePrevPage}
+      ></Pagination>
+      <div className="audio-player"></div>
     </div>
   );
 };
