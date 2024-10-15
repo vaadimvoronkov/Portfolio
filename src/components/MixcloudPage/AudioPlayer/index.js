b43817dbe5ddad11acc6cf4afa4eb087603f23a1
vaadimvoronkov <vaadimvoronkov@gmail.com>
2024-10-15 13:28:14 +0700

    Добавление состояний в компонент аудио

--- src/components/MixcloudPage/AudioPlayer/index.js
+++ src/components/MixcloudPage/AudioPlayer/index.js
@@ -1,6 +1,20 @@
 import "./styles.css";
+import React, { useState, useEffect, useRef } from 'react';
 
-const AudioPlayer = () => {
+
+const AudioPlayer = (props) => {
+  const [trackIndex, setTrackIndex] = useState(0);
+  const [trackProgress, setTrackProgress] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const{audiofile_url, title, } = props;
+
+  const audioRef = useRef(new Audio(audiofile_url));
+  const intervalRef = useRef();
+  const isReady = useRef(false);
+
+  const {duration} = audioRef.current;
+
   return <div className="audio-player"></div>;
 };
 
